507c5679de38e8a573240feaead03f7460d57fb4
Nipunn Koorapati <nipunn1313@gmail.com>
2022-12-10 12:58:13 -0500

    Add auth

--- convex/oncall.ts
+++ convex/oncall.ts
@@ -1,7 +1,9 @@
 import { mutation, query } from './_generated/server'
 import { Document } from "../convex/_generated/dataModel";
+import { Auth } from "convex/server";
 
-export const updateOncallMembers = mutation(async ({ db }, users: any[]) => {
+export const updateOncallMembers = mutation(async ({ db, auth }, users: any[]) => {
+  await checkIdentity(auth);
   for (const user of users) {
     const current = await db.query("oncallMembers").filter(q => q.eq(q.field("id"), user.id)).first();
     if (current) {
@@ -12,7 +14,8 @@ export const updateOncallMembers = mutation(async ({ db }, users: any[]) => {
   }
 })
 
-export const updateCurrentOncall = mutation(async ({ db }, user: any) => {
+export const updateCurrentOncall = mutation(async ({ db, auth }, user: any) => {
+  await checkIdentity(auth);
   const prev = await db.query("currentOncall").first();
   if (prev) {
     db.delete(prev._id);
@@ -22,6 +25,24 @@ export const updateCurrentOncall = mutation(async ({ db }, user: any) => {
   await db.insert("currentOncall", {memberId: current._id});
 })
 
-export const getMembers = query(async ({db}): Promise<Document<"oncallMembers">[]> => {
+export const getMembers = query(async ({db, auth}): Promise<Document<"oncallMembers">[]> => {
+  await checkIdentity(auth);
   return await db.query("oncallMembers").collect();
 })
+
+
+async function checkIdentity(auth: Auth) {
+  const identity = await auth.getUserIdentity();
+  if (!identity) {
+    throw new Error("Unauthenticated call");
+  }
+  if (!identity.email) {
+    throw new Error("Requires email");
+  }
+  if (!identity.email.endsWith("@convex.dev")) {
+    throw new Error("Must have @convex.dev email");
+  }
+  if (!identity.emailVerified) {
+    throw new Error("Email must be verified");
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import { FormEvent, useEffect, useState } from 'react'
 import { useMutation, useQuery, useAction } from '../convex/_generated/react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 
 export default function App() {
+  return (
+    <main>
+      <h1>Convex Oncall</h1>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Members/>
+      </ErrorBoundary>
+			<LogoutButton/><br/>
+      <span>Source: https://github.com/nipunn1313/oncall_app</span><br/>
+      <span>TODOs</span><br/>
+      <span>Include users not in rotation?</span><br/>
+      <span>Include the identity of the person paging in the page details</span><br/>
+      <span>Get colors working better</span><br/>
+      <span>Flex boxes?</span><br/>
+      <span>Slack integration</span><br/>
+      <span>Favicon</span><br/>
+    </main>
+  )
+}
+
+function ErrorFallback({error}: FallbackProps) {
+  return (
+    <>
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </>
+  )
+}
+
+function Members() {
   const members = useQuery('oncall:getMembers') || []
   const page = useAction('actions/page')
 
@@ -19,9 +50,9 @@ export default function App() {
   console.log(members);
 
   return (
-    <main>
-      <h1>Convex Oncall</h1>
+    <>
       <p className="badge">
+        <UserName/><br/>
         <span>Last synced: {lastSynced}</span>
       </p>
       <ul>
@@ -34,14 +65,23 @@ export default function App() {
           </li>
         ))}
       </ul>
-      <span>TODOs</span><br/>
-      <span>Require login</span><br/>
-      <span>Include users not in rotation?</span><br/>
-      <span>Include the identity of the person paging in the page details</span><br/>
-      <span>Get colors working better</span><br/>
-      <span>Flex boxes?</span><br/>
-      <span>Slack integration</span><br/>
-      <span>Favicon</span><br/>
-    </main>
+    </>
   )
+}
+
+function LogoutButton() {
+  const { logout } = useAuth0();
+  return (
+    <button
+      className="btn btn-primary"
+      onClick={() => logout({ returnTo: window.location.origin })}
+    >
+      Log out
+    </button>
+  );
+}
+
+function UserName() {
+  const { user } = useAuth0();
+  return <>Logged in{user!.email ? ` as ${user!.email}` : ""}</>;
 }
