eaaf24ad5cae18252f8132726f4c599d218504f1
Nipunn Koorapati <nipunn1313@gmail.com>
2023-01-30 12:17:09 -0800

    Update to convex 0.7.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import type { AppProps } from 'next/app'
 
 import { ConvexProviderWithAuth0 } from 'convex/react-auth0'
 import { ConvexReactClient } from 'convex/react'
+
 import convexConfig from "../convex.json";
-import clientConfig from '../convex/_generated/clientConfig'
-const convex = new ConvexReactClient(clientConfig)
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 const authInfo = convexConfig.authInfo[0];
 
 function MyApp({ Component, pageProps }: AppProps) {
